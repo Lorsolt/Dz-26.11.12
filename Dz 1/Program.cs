11c430109bea1c1469de1bb1f7613a745b2e20cb
Lorsolt <mr.lorsolt@mail.ru>
2022-12-03 12:51:25 +0300

    решено

---
+++ Dz 1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+Console.WriteLine("Размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] num = new int[size];
+
+ArrayRandomNumbers(num);
+
+Console.WriteLine("Массив: ");
+
+PrintArray(num);
+int count = 0;
+
+for (int c = 0; c < num.Length; c++)
+    if (num[c] % 2 == 0)
+        count++;
+
+Console.WriteLine($"Чисел {num.Length} , {count} чётных");
+
+void ArrayRandomNumbers(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        num[i] = new Random().Next(100, 999);
+    }
+}
+void PrintArray(int[] num)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < num.Length; i++)
+    {
+        Console.Write(num[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
---
+++ Dz 2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+Console.WriteLine("Размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = new int[size];
+
+ArrayRandomNumbers(numbers);
+
+Console.WriteLine("Массив: ");
+
+PrintArray(numbers);
+
+int sum = 0;
+
+for (int k = 0; k < numbers.Length; k += 2)
+    sum = sum + numbers[k];
+
+Console.WriteLine($"всего {numbers.Length} чисел, сумма  нечётных позиций = {sum}");
+
+void ArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(-99, 99);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
